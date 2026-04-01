c6e897b20549430bdfbed259bff8e55bff1e44cc
Rika4698 <rekasharmin46@gmail.com>
2026-04-01 08:42:32 +0600

    Footer

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/Footer";
 import { Navbar } from "@/components/Navbar";
 
 
@@ -6,6 +7,7 @@ export default function Home() {
   
       <main className="relative min-h-screen">
         <Navbar/>
+        <Footer/>
         
       </main>
     
